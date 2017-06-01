d7773938799c1b96302be17a6c35b2708975cf07
AISDhaval <211379@ais.edu.hk>
2017-06-02 00:11:09 +0800

    v3

--- sketch.js
+++ sketch.js
@@ -1,43 +1,31 @@
-var numberOfPictures = 8;
-var doorTransition = new Array(numberOfPictures);
-var startAnimate;
-var animationSpeed;
-var myFrameCount;
+//var numberOfPictures = 8;
+//var doorTransition = new Array(numberOfPictures);
+//var startAnimate;
+//var animationSpeed;
+//var myFrameCount;
+var lock;
 
 function preload()
 {
   //images
   door = loadImage('https://dl.dropboxusercontent.com/s/8ck06heei1runy9/door.png');
   doorlock = loadImage('https://dl.dropboxusercontent.com/s/7ma7vss4opzhzl6/lock.png');
-  
-  //Canvas 1 change to Canvas 2
-  doorTransition[0] = loadImage('https://dl.dropboxusercontent.com/s/gt4frzg07flp5ng/Screen%20Shot%201.png');
-  doorTransition[1] = loadImage('https://dl.dropboxusercontent.com/s/fi9rw2ermm6evo2/Screen%20Shot%202.png');
-  doorTransition[2] = loadImage('https://dl.dropboxusercontent.com/s/1o96utixr6h2oqj/Screen%20Shot%203.png');
-  doorTransition[3] = loadImage('https://dl.dropboxusercontent.com/s/03gsuea75slnrv7/Screen%20Shot%204.png');
-  doorTransition[4] = loadImage('https://dl.dropboxusercontent.com/s/012gcddyqmx278j/Screen%20Shot%205.png');
-  doorTransition[5] = loadImage('https://dl.dropboxusercontent.com/s/a9ga103hvpqtcu0/Screen%20Shot%206.png');
-  doorTransition[6] = loadImage('https://dl.dropboxusercontent.com/s/r1w3rsu990l3z6u/Screen%20Shot%207.png');
-  doorTransition[7] = loadImage('https://dl.dropboxusercontent.com/s/jt5xzde6ht1mt69/Screen%20Shot%208.png');
-  doorTransition[8] = loadImage('https://dl.dropboxusercontent.com/s/mklzwzmbir7wv35/Screen%20Shot%209.png');
-  doorTransition[9] = loadImage('https://dl.dropboxusercontent.com/s/o8mixpqya363219/Screen%20Shot%2010.png');
+  lockenvelope = loadImage('https://dl.dropboxusercontent.com/s/miewuq46ggkref2/Lock.jpg');
+  envelope = loadImage('https://dl.dropboxusercontent.com/s/x17zl4easw14z9s/Envelope.jpg');
 }
 
+
 function setup()
-{  
-  createCanvas(1000,600);
-  canvas = 1;
+{
+  createCanvas(800,600);
+  canvas = 0;
   lock = false;
-  
-  startAnimate = 0;
-  animationSpeed = 5;
-  myFrameCount = 0;
 }
 
 function draw()
 {
   background(155,155,155);
- /*
+
   if (canvas == 0)
   {
     canvas1();
@@ -46,103 +34,280 @@ function draw()
   {
     canvas2();
   }
-  */
-  if (startAnimate == 0)
+  else if (canvas == 2)
   {
-    image(doorTransition[0],0,0,1000,600);
+    canvas3();
   }
-
-  if (mouseX > 535 && mouseX < 535 + 80 && mouseY > 350 && mouseY < 350 + 88  )
+  else
   {
-    cursor(HAND);
-    if (mouseIsPressed == true)
-    {
-      if (startAnimate == 0)
-      {
-        startAnimate =2; 
-      }
-      else if (startAnimate == 2) 
-      {
-        startAnimate = 0; 
-      }
-      lock = true;
-    }
+    cursor(ARROW);
+  }
+}
+
+function canvas1()
+{
+  image(lockenvelope,0,0,800,600);
+
+  if(mouseX > 480 && mouseX < 480 + 160 && mouseY > 30 && mouseY < 30 + 88  )
+  {
+          cursor(HAND);
+
+          if ( lock == false && mouseIsPressed == true)
+          {
+
+                  if (canvas == 0)
+                  {
+                    canvas = 1;
+                  }
+                  else if (canvas == 1)
+                  {
+                    canvas = 0;
+                  }
+                  lock = true;
+          }
+
+
+   }
+   else if(mouseX > 460 && mouseX < 460 + 160 && mouseY > 140 && mouseY < 140 + 400  )
+   {
+         cursor(HAND);
+
+         if ( lock == false && mouseIsPressed == true)
+         {
+
+               if (canvas == 0)
+               {
+                 canvas = 2;
+               }
+               else if (canvas == 2)
+               {
+                 canvas = 0;
+               }
+               lock = true;
+         }
   }
   else
   {
     cursor(ARROW);
   }
 
-  if (startAnimate == 1)
-  {
-    animateSequence();
-  }
-
-  if (startAnimate == 2)
-  {
-    image(doorTransition[9],0,0,1000,600);
-  }
 }
 
-
-/*function canvas1()
+/*function userInterface(a,b,c,d,pw1,pw2,pw3,pw4)
 {
-  image(door,400,200,200,400);
-  image(doorlock,535,350,80,88);
-  noStroke();
-  fill(0,0,0);
-  rect(380,200,20,400);
-  rect(380,180,220,20);
-  rect(600,180,20,420);
-  
-  if(mouseX > 535 && mouseX < 535 + 80 && mouseY > 350 && mouseY < 350 + 88  )
+  textSize(20);
+  strokeWeight(5);
+  stroke(0,0,0);
+  fill(200,200,200);
+  rect(a,a/10,b,b-100);
+  rect(a,a,b,b);
+  strokeWeight(3);
+  fill(255,255,255);
+  rect(c+15,a-75,50,50);
+  rect(c+70,a-75,50,50);
+  rect(c+125,a-75,50,50);
+  rect(c+180,a-75,50,50);
+  rect(c,c,d,d);
+  fill(230,230,230);
+
+  //1
+  fill(230,230,230);
+  if (mouseX >= c && mouseX <= c+(d/3) && mouseY >= c && mouseY <= c+(d/3))
+  {
+    fill(150,150,150);
+  }
+  rect(c,c,d/3,d/3);
+  text("1",c+(c/3),c+(c/3));
+  fill(230,230,230);
+  //2
+  if (mouseX >= c+(d/3) && mouseX <= c+(2*d/3) && mouseY >= c && mouseY <= c+(d/3))
+  {
+    fill(150,150,150);
+  }
+  rect(c+(d/3),c,d/3,d/3);
+  text("2",c+(3*c/3),c+(c/3));
+  fill(230,230,230);
+  //3
+  if (mouseX >= c+(2*d/3) && mouseX <= c+(3*d/3) && mouseY >= c && mouseY <= c+(d/3))
+  {
+    fill(150,150,150);
+  }
+  rect(c+((d*2)/3),c,d/3,d/3);
+  text("3",c+(5*c/3),c+(c/3));
+  fill(230,230,230);
+  //4
+  if (mouseX >= c && mouseX <= c+(d/3) && mouseY >= c+(d/3) && mouseY <= c+(2*d/3))
+  {
+    fill(150,150,150);
+  }
+  rect(c,c+(d/3),d/3,d/3);
+  text("4",c+(c/3),c+(3*c/3));
+  fill(230,230,230);
+  //5
+  if (mouseX >= c+(d/3) && mouseX <= c+(2*d/3) && mouseY >= c+(d/3) && mouseY <= c+(2*d/3))
+  {
+    fill(150,150,150);
+  }
+  rect(c+(d/3),c+(d/3),d/3,d/3);
+  text("5",c+(3*c/3),c+(3*c/3));
+  fill(230,230,230);
+  //6
+  if (mouseX >= c+(2*d/3) && mouseX <= c+(3*d/3) && mouseY >= c+(d/3) && mouseY <= c+(2*d/3))
+  {
+    fill(150,150,150);
+  }
+  rect(c+((d*2)/3),c+(d/3),d/3,d/3);
+  text("6",c+(5*c/3),c+(3*c/3));
+  fill(230,230,230);
+  //7
+  if (mouseX >= c && mouseX <= c+(d/3) && mouseY >= c+(2*d/3) && mouseY <= c+d)
+  {
+    fill(150,150,150);
+  }
+  rect(c,c+((d*2)/3),d/3,d/3);
+  text("7",c+(c/3),c+(5*c/3));
+  fill(230,230,230);
+  //8
+  if (mouseX >= c+(d/3) && mouseX <= c+(2*d/3) && mouseY >= c+(2*d/3) && mouseY <= c+d)
+  {
+    fill(150,150,150);
+  }
+  rect(c+(d/3),c+((d*2)/3),d/3,d/3);
+  text("8",c+(3*c/3),c+(5*c/3));
+  fill(230,230,230);
+  //9
+  if (mouseX >= c+(2*d/3) && mouseX <= c+(3*d/3) && mouseY >= c+(2*d/3) && mouseY <= c+d)
+  {
+    fill(150,150,150);
+  }
+  rect(c+((d*2)/3),c+((d*2)/3),d/3,d/3);
+  text("9",c+(5*c/3),c+(5*c/3));
+  fill(230,230,230);
+
+
+  //HAND
+  //1
+  if (mouseX >= c && mouseX <= c+(d/3) && mouseY >= c && mouseY <= c+(d/3))
   {
     cursor(HAND);
-    
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //2
+  else if (mouseX >= c+(d/3) && mouseX <= c+(2*d/3) && mouseY >= c && mouseY <= c+(d/3))
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //3
+  else if (mouseX >= c+(2*d/3) && mouseX <= c+(3*d/3) && mouseY >= c && mouseY <= c+(d/3))
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //4
+  else if (mouseX >= c && mouseX <= c+(d/3) && mouseY >= c+(d/3) && mouseY <= c+(2*d/3))
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //5
+  else if (mouseX >= c+(d/3) && mouseX <= c+(2*d/3) && mouseY >= c+(d/3) && mouseY <= c+(2*d/3))
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //6
+  else if (mouseX >= c+(2*d/3) && mouseX <= c+(3*d/3) && mouseY >= c+(d/3) && mouseY <= c+(2*d/3))
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //7
+  else if (mouseX >= c && mouseX <= c+(d/3) && mouseY >= c+(2*d/3) && mouseY <= c+d)
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //8
+  else if (mouseX >= c+(d/3) && mouseX <= c+(2*d/3) && mouseY >= c+(2*d/3) && mouseY <= c+d)
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  //9
+  else if (mouseX >= c+(2*d/3) && mouseX <= c+(3*d/3) && mouseY >= c+(2*d/3) && mouseY <= c+d)
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  else if (mouseX >= 1 && mouseX <= 91 && mouseY >= 100 && mouseY <= 150 && accessGranted == 0)
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      lock = 1;
+    }
+  }
+  else if (mouseX >= 1 && mouseX <= 91 && mouseY >= 300 && mouseY <= 350 && accessGranted == 0)
+  {
+    cursor(HAND);
+    if (mouseIsPressed)
+    {
+      startTime = 1;
+    }
+  }
+  else
+  {
+    cursor(ARROW);
+  }
+}
+*/
+function canvas2()
+{
+  background(255,0,0);
+  image(envelope,0,0,800,600);
+
+  if(mouseX > 0 && mouseX < 0 + 800 && mouseY > 0 && mouseY < 0 + 600  )
+  {
+    cursor(HAND);
+
     if ( lock == false && mouseIsPressed == true)
     {
-      
-      if (canvas == 0)
-      {
-        canvas = 1; 
-      }
-      else if (canvas == 1) 
-      {
-        canvas = 0; 
-      }
-      lock = true;
-    }
-  }
-
-  else
-  {
-    cursor(ARROW);
-  }
-  
-}
-*/
-
-
-/*function canvas2()
-{
-  background(0,0,0);
-  image(doorlock,-25,100,1000,1000);
-  itemGrid();
-  
-  if(mouseX > 535 && mouseX < 535 + 80 && mouseY > 350 && mouseY < 350 + 88  )
-  {
-    cursor(HAND);
-
-    if ( lock == false && mouseIsPressed == true)
-    {
 
       if (canvas == 0)
       {
-        canvas = 1; 
+        canvas = 1;
       }
-      else if (canvas == 1) 
+      else if (canvas == 1)
       {
-        canvas = 0; 
+        canvas = 0;
       }
       lock = true;
     }
@@ -152,34 +317,17 @@ function draw()
   {
     cursor(ARROW);
   }
-  
-}
-*/
 
-/*
-function itemGrid()
+}
+
+
+function canvas3()
 {
-  // background for item grid
-  fill(50,50,50);
-  rect(220,20,560,90);
+  background(255,0,0);
 
-  // first digit box
-  fill(125,125,125);
-  rect(340,30,70,70);
-
-  // second digit box
-  fill(125,125,125);
-  rect(420,30,70,70);
-
-  // third digit box
-  fill(125,125,125);
-  rect(500,30,70,70);
-  // fourth digit box
-  fill(125,125,125);
-  rect(580,30,70,70);
-
+  userInterface();
 }
-*/
+
 
 function mouseReleased()
 {
@@ -187,63 +335,4 @@ function mouseReleased()
   {
     lock = false;
   }
-} 
-
-function animateSequence()
-{
-
-
-  if (myFrameCount % 40 < 5 && lock == false && mouseX > 535 && mouseX < 535 + 80 && mouseY > 350 && mouseY < 350 + 88  )
-  {
-    image(doorTransition[0],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 5 && myFrameCount % 40 < 10)
-  {
-    image(doorTransition[1],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 10 && myFrameCount % 40 < 15)
-  {
-    image(doorTransition[2],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 15 && myFrameCount % 40 < 20)
-  {
-    image(doorTransition[3],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 20 && myFrameCount % 40 < 25)
-  {
-    image(doorTransition[4],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 25 && myFrameCount % 40 < 30)
-  {
-    image(doorTransition[5],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 30 && myFrameCount % 40 < 35)
-  {
-    image(doorTransition[6],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 35 && myFrameCount % 40 < 40)
-  {
-    image(doorTransition[7],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 40 && myFrameCount % 40 < 45)
-  {
-    image(doorTransition[8],0,0,1000,600);
-  }
-  else if (myFrameCount % 40 >= 45 && myFrameCount % 40 < 50)
-  {
-    image(doorTransition[9],0,0,1000,600);
-    rect(100,100,400,400);
-  }
-
-
-  myFrameCount++;
-  if (myFrameCount> 35)
-    startAnimate = 2;
-  
 }
-
-
-
-
-
-
